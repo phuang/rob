17d5219ac8b70ea5dea9652fc371b0cd9f429cbd
Peng Huang <shawn.p.huang@gmail.com>
2013-01-25 22:45:05 -0500

    wip

--- tools/generator.py
+++ tools/generator.py
@@ -2,11 +2,6 @@ class Generator(object):
   def __init__(self):
     object.__init__(self)
 
-  def Generate(self, clazz):
-    out = []
-    self.GenerateMetaData(clazz, out)
-    return '\n'.join(out)
-
   def GenerateMetaData(self, clazz, out):
     METHOD_SIZE = 4
     n_method = len(clazz.slots) + len(clazz.signals)
@@ -15,8 +10,8 @@ class Generator(object):
 
     meta_string_array = []
     meta_string_map = {}
+    # Have to use an array, so the nested function can access it
     meta_string_offset = [0]
-
     def AddString(s):
       if s not in meta_string_map:
         meta_string_array.append(s)
@@ -33,7 +28,6 @@ class Generator(object):
     out.append('  %3d, %3d,  // properties' % (n_property,
         offset if n_property else 0))
     out.append('  %3d,       // signal count' % len(clazz.signals))
-    out.append('')
 
     def GenerateFunc(f):
       out.append('  %3d, %3d, %3d, %3d,' % (
@@ -47,13 +41,11 @@ class Generator(object):
       out.append('  // signals: name, signature, parameters, type')
       for s in clazz.signals:
         GenerateFunc(s)
-      out.append('')
 
     if clazz.slots:
       out.append('  // slot: name, signature, parameters, type')
       for s in clazz.slots:
         GenerateFunc(s)
-      out.append('')
 
     if clazz.properties:
       out.append('  // property: name, type')
@@ -62,13 +54,12 @@ class Generator(object):
           AddString(p.name),
           AddString(p.type.name),
         ))
-      out.append('')
     out.append('  %3d,  //eod' % 0)
 
     out.append('};')
     out.append('')
 
-    out.append('static const char meta_stringdata_%s[] = {' % clazz.name)
+    out.append('static const char meta_string_data_%s[] = {' % clazz.name)
     for s in meta_string_array:
       out.append('  "%s\\0"' % s)
     out.append('};')
@@ -76,9 +67,50 @@ class Generator(object):
 
     return '\n'.join(out)
 
-  def GenerateAllStrings(self):
-    self.AddString(self.class_def.name)
-    for slot in self.class_def.slots:
-      self.AddString()
+  def GenerateMetaObject(self, clazz, out):
+    out.append('const MetaObject %s::static_meta_object = {' % clazz.name)
+    out.append('  &%s::static_meta_object,' % clazz.parent)
+    out.append('  meta_string_data_%s,' % clazz.name)
+    out.append('  meta_data_%s' % clazz.name)
+    out.append('};')
+    out.append('')
+    out.append('const MetaObject* %s::meta_object() const {' % clazz.name)
+    out.append('  return &static_meta_object;')
+    out.append('}')
+    out.append('')
+    out.append('void* %s::meta_cast(const char* class_name) {' % clazz.name)
+    out.append('  if (!class_name) return 0;')
+    out.append('  if (!strcmp(class_name, meta_string_data_%s))' % clazz.name)
+    out.append('    return static_cast<void*>(const_cast<%s*>(this));' % clazz.name)
+    out.append('  return %s::meta_cast(class_name);' % clazz.parent)
+    out.append('}')
+    out.append('')
+    out.append('int %s::meta_call(MetaObject::Call c, int id, void **a) {' % clazz.name)
+    out.append('  id = %s::meta_call((c, id, a);' % clazz.parent)
+    out.append('  if (id < 0) return id;')
+    out.append('  switch(c) {')
+    out.append('    case MetaObject::INVOKE_META_METHOD: {')
+    out.append('      break;');
+    out.append('    }');
+    out.append('    case MetaObject::READ_PROPERTY: {')
+    out.append('      break;');
+    out.append('    }');
+    out.append('    case MetaObject::WRITE_PROPERTY: {')
+    out.append('      break;');
+    out.append('    }');
+    out.append('  }');
+    out.append('  return id;')
+    out.append('}')
+    out.append('')
+
+  def GenerateMetaFunc(self, clazz, out):
+    pass
+
+  def Generate(self, clazz):
+    out = []
+    self.GenerateMetaData(clazz, out)
+    self.GenerateMetaObject(clazz, out)
+    return '\n'.join(out)
+
 
 
